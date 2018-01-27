9c115d7594e322ffd9534e48368fc873bd552232
Olivier Cervello <olivier.cervello@gmail.com>
2018-01-27 16:00:48 -0600

    Update docker-compose and requirements

--- meloshare/api/config.py
+++ meloshare/api/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = 'verysecretapistuff'
-    DEBUG = True
+    DEBUG = False
     SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite://')
     CACHE_CONFIG = {
         'CACHE_TYPE': 'redis',

18088fcb0dc0352291b5b2b7cba3a13580334fa1
Olivier Cervello <ocervell@hawk.iit.edu>
2018-01-26 18:47:21 -0600

    Update __init__.py

--- meloshare/ui/__init__.py
+++ meloshare/ui/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from config import config
 
-def create_app(profile='developpement'):
+def create_app(profile='development'):
   app = Flask(__name__)
   cfg = config[profile]
   app.config_from_object(cfg)

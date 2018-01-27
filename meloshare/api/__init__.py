a47040e54b59b216d86815de0a8c66ca8b32f38b
Olivier Cervello <ocervell@hawk.iit.edu>
2018-01-26 19:14:01 -0600

    Update __init__.py

--- meloshare/api/__init__.py
+++ meloshare/api/__init__.py
@@ -1,1 +1,8 @@
+from flask_flash import Flash
+from config import config
+from extensions import EXTENSIONS
+from resources import RESOURCES
 
+def create_app(profile='development'):
+    app = Flash(resources=RESOURCES, config=config, profile=profile)
+    return app
